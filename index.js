1fdb632a14fd096061d33cd427777d74819ce336
srushti0415 <srushtikulkarni15@gmail.com>
2021-09-17 22:36:21 +0530

    Added Even or Odd program

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("srushtikulkarni")
+function EvenorOdd(a) {
+    if (a % 2 == 0 ) {
+      console.log ("The no. is even")
+    }
+    else {
+        console.log("The no. is odd")
+    }
+}
+EvenorOdd(4)
