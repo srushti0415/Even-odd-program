7e4001ecd72e20e1364edd3792e952110ff5a4a8
srushti0415 <srushtikulkarni15@gmail.com>
2021-09-19 23:10:37 +0530

    trueorfalse

---
+++ index2.js
@@ -0,0 +1,11 @@
+console.log("hi how u doing")
+function trueorfalse(a) {
+    if (a  >= 10) {
+      console.log ("The no. is true")
+    }
+    else {
+        console.log("The no. is false")
+    }
+}
+trueorfalse(4)
+
