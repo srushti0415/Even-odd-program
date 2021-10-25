79590bb1991813d5b2bd667bc6e907e3cb757c0f
srushti0415 <srushtikulkarni15@gmail.com>
2021-10-25 22:40:51 +0530

    updated

--- index1.js
+++ index1.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+function addtwonum() {
+    var num1 = parseInt(document.getElementById('number1').value);
+    var num2 = parseInt(document.getElementById('number2').value);
+    var ans = document.getElementById('answer');
+    ans.value = num1+num2;
+    
+
+}
---
+++ indexs.js
@@ -0,0 +1,7 @@
+function Sub() {
+    var a = document.getElementById('num1').value;
+    var b = document.getElementById('num2').value;
+    var c = document.getElementById('ans')
+    ans.value = a - b;
+
+}
